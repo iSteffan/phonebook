8d8f816e8704b11537bcd6a1920e57fe474a361c
Stepan Skichko <st.skichko@gmail.com>
2023-05-23 15:55:38 +0300

    add auth operations

---
+++ src/components/Navigation/Navigation.jsx
@@ -0,0 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+export const Navigation = () => {
+  return (
+    <Routes>
+      {/* <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="movies" element={<Movies />} />
+        <Route path="movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
+        <Route path="*" element={<Home />} />
+      </Route> */}
+    </Routes>
+  );
+};
