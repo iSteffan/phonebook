3d4ffc1b1fd4b1195de09cd5c249d895d1e22cc7
Stepan Skichko <st.skichko@gmail.com>
2023-05-28 13:23:32 +0300

    modal window in process

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -4,14 +4,45 @@ import {
   ContactName,
   ContactPhone,
 } from './ContactList.styled';
+import {
+  Form,
+  Field,
+  ErrorMessageName,
+  ErrorMessageNumber,
+} from '../ContactForm/ContactForm.styled';
+import {
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  Icon,
+  InputLeftElement,
+  FormControl,
+  FormLabel,
+  InputGroup,
+  Input,
+} from '@chakra-ui/react';
 import PropTypes from 'prop-types';
 import { FiUser } from 'react-icons/fi';
 import { BsTelephone } from 'react-icons/bs';
-import { ButtonGroup, IconButton } from '@chakra-ui/button';
+import { Button, ButtonGroup, IconButton } from '@chakra-ui/button';
 import { AiOutlineDelete } from 'react-icons/ai';
 import { GrConfigure } from 'react-icons/gr';
+import { AiOutlineUserAdd } from 'react-icons/ai';
+import { Formik } from 'formik';
+// import * as Yup from 'yup';
+// import { nanoid } from 'nanoid';
+import { patchContact } from 'redux/contact/contactsOperations';
+import { useDispatch } from 'react-redux';
 
 export const ContactList = ({ contacts, onDelete }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const dispatch = useDispatch();
+
   return (
     <List>
       {contacts.map(contact => {
@@ -30,6 +61,7 @@ export const ContactList = ({ contacts, onDelete }) => {
                 colorScheme="blue"
                 aria-label="change contact"
                 icon={<GrConfigure color="white" />}
+                onClick={onOpen}
               ></IconButton>
               <IconButton
                 colorScheme="gray"
@@ -38,6 +70,91 @@ export const ContactList = ({ contacts, onDelete }) => {
                 onClick={() => onDelete(contact.id)}
               />
             </ButtonGroup>
+            <Modal isOpen={isOpen} onClose={onClose}>
+              <ModalOverlay />
+              <ModalContent>
+                <ModalHeader>Редагування контакту</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                  <Formik
+                    initialValues={{
+                      name: contact.name,
+                      number: contact.number,
+                    }}
+                    // validationSchema={FormSchema}
+                    onSubmit={(values, actions) => {
+                      // onSave({ ...values, id: nanoid() });
+                      actions.resetForm();
+                    }}
+                  >
+                    {props => {
+                      const isErrorName =
+                        !props.values.name && props.touched.name;
+                      const isErrorNumber =
+                        !props.values.number && props.touched.number;
+
+                      return (
+                        <Form>
+                          <Field name="name">
+                            {({ field }) => (
+                              <FormControl isInvalid={isErrorName} mb={3}>
+                                <FormLabel>Name</FormLabel>
+                                <InputGroup>
+                                  <Input
+                                    {...field}
+                                    variant="filled"
+                                    placeholder="Enter name"
+                                  />
+                                  <InputLeftElement pointerEvents="none">
+                                    <Icon as={FiUser} color="grey.800" />
+                                  </InputLeftElement>
+                                </InputGroup>
+                              </FormControl>
+                            )}
+                          </Field>
+                          <ErrorMessageName name="name" component="div" />
+                          <Field name="number">
+                            {({ field }) => (
+                              <FormControl isInvalid={isErrorNumber}>
+                                <FormLabel>Number</FormLabel>
+                                <InputGroup size="md">
+                                  <Input
+                                    {...field}
+                                    pr="4.5rem"
+                                    variant="filled"
+                                    placeholder="Enter number"
+                                    type="tel"
+                                  />
+                                  <InputLeftElement pointerEvents="none">
+                                    <Icon as={BsTelephone} color="grey.800" />
+                                  </InputLeftElement>
+                                </InputGroup>
+                              </FormControl>
+                            )}
+                          </Field>
+                          <ErrorMessageNumber name="number" component="div" />
+                          <Button
+                            leftIcon={<AiOutlineUserAdd />}
+                            mt={4}
+                            colorScheme="blue"
+                            isLoading={props.isSubmitting}
+                            type="submit"
+                          >
+                            Add contact
+                          </Button>
+                        </Form>
+                      );
+                    }}
+                  </Formik>
+                </ModalBody>
+
+                <ModalFooter>
+                  <Button colorScheme="blue" mr={3} onClick={onClose}>
+                    Close
+                  </Button>
+                </ModalFooter>
+              </ModalContent>
+            </Modal>
           </ListItem>
         );
       })}
