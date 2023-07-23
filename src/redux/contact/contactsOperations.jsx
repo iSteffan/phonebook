2e3bc9e8e0ccaf2b9df09d8fcbf66b4ed86ec102
Stepan Skichko <st.skichko@gmail.com>
2023-07-23 10:52:52 +0300

    change backend routes

--- src/redux/contact/contactsOperations.jsx
+++ src/redux/contact/contactsOperations.jsx
@@ -1,13 +1,13 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://connections-api.herokuapp.com/';
+axios.defaults.baseURL = 'https://phonebook-backend-m2k3.onrender.com/';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
   async (_, thunkAPI) => {
     try {
-      const { data } = await axios.get('/contacts');
+      const { data } = await axios.get('/api/contacts');
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -19,7 +19,7 @@ export const addContact = createAsyncThunk(
   'contacts/addContact',
   async (contact, thunkAPI) => {
     try {
-      const { data } = await axios.post('/contacts', contact);
+      const { data } = await axios.post('/api/contacts', contact);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -31,7 +31,7 @@ export const deleteContact = createAsyncThunk(
   'contacts/deleteContact',
   async (contactId, thunkAPI) => {
     try {
-      const { data } = await axios.delete(`/contacts/${contactId}`);
+      const { data } = await axios.delete(`/api/contacts/${contactId}`);
       return data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
@@ -43,7 +43,7 @@ export const editContact = createAsyncThunk(
   'contacts/editContact',
   async (data, { rejectWithValue }) => {
     try {
-      const response = await axios.patch(`/contacts/${data.id}`, {
+      const response = await axios.patch(`/api/contacts/${data.id}`, {
         name: data.name,
         number: data.number,
       });
